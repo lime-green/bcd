cdf6b829069dbbecdd5a260e0491b945a722262a
Joshua Doncaster-Marsiglio <jmarsiglio@gmail.com>
2014-11-17 15:01:45 +0000

    Added source files

---
+++ back_directory.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+#
+# Outputs the cd command to go to the right-most matched directory (prioritises directories other than the absolute right-most directory)
+# Accepts regular expressions--just remember to enclose in single quotes if using shell special characters (*,!,etc.)
+#
+# USAGE: 
+# function bcd(){ eval $(back_directory.pl $1); }
+#
+# EXAMPLE: if pwd = /users/tor/arqt/qko/joshua/scripts
+# bcd qko ==> cd /users/tor/arqt/qko
+# bcd t   ==> cd /users/tor/arqt
+# bcd t[oa] ==> cd /users/tor
+#
+# Joshua Doncaster-Marsiglio
+# Nov 2014
+#
+
+use strict;
+use warnings;
+
+# using getopt slows down the program considerably (by a factor of two)
+#use Getopt::Long; # parses options
+
+# debug option is for testing purposes (now used by providing a second argument as path)
+#my $debug;
+#GetOptions('debug|d' =>\$debug);
+
+my $arg_c = $#ARGV + 1;
+
+if ($arg_c != 1 && $arg_c != 2)
+{
+   print "Invalid number of arguments\n";
+   exit 1;
+}
+
+my $cwd =  defined $ARGV[1] ? $ARGV[1] : `pwd;`;
+my $input = $ARGV[0];
+my ($parent_cwd) = $cwd =~ /(.*\/).*$/; #removes last directory (gets parent directory full path)
+my $match;
+
+# input pattern has a trailing slash and match found
+if ($input =~ /\/$/ && (($match) = $parent_cwd =~ /(.*$input)/))
+{
+    print 'cd ', $match;
+    exit 0;
+}
+# input pattern has no trailing slash and match found
+elsif (($match) = $parent_cwd =~ /(.*$input.*?\/)/)
+{
+    print 'cd ', $match;
+    exit 0;
+}
+
+# is the current directory or no match found - do nothing
+else
+{
+    exit 0;
+}
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Install script for bcd
+
+# check if the current user's bin is in path 
+echo "Checking path..."
+echo "$PATH" | tr ':' '\n' | grep "^$HOME/bin$" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo -e "~/bin is not in your path. Please add the following line to your .profile:\n export \$PATH=\$PATH:~/bin" 1>&2
+    echo -e "Then run: source ~/.profile" 1>&2
+    exit 1
+fi
+
+# copy file to current user's bin
+echo "Copying file..."
+mkdir -p $HOME/bin
+cp back_directory.pl $HOME/bin
+
+# write function and alias to bashrc
+echo "Writing to .bashrc..."
+cat <<'BASHRC' >> $HOME/.bashrc
+bcd() {
+cmd=`back_directory.pl $1`;
+if [[ $? -eq 0 ]]; then
+    eval $cmd;
+else
+    echo "USAGE: ${FUNCNAME[0]} pattern" 1>&2
+    echo -e "\nSearches the current directory path from right to left and changes directory to the first matched pattern" 1>&2
+fi
+}
+
+alias bd='bcd'
+BASHRC
+
+echo -e "\nDone" 
